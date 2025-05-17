c4c115636076a48ca528e745d0010fc954990024
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-05-17 22:05:15 +0000

    se actualizo app.py para configurar JWT

--- src/app.py
+++ src/app.py
@@ -1,3 +1,78 @@
+# """
+# This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+# """
+# import os
+# from flask import Flask, request, jsonify, url_for, send_from_directory
+# from flask_migrate import Migrate
+# from flask_swagger import swagger
+# from api.utils import APIException, generate_sitemap
+# from api.models import db
+# from api.routes import api
+# from api.admin import setup_admin
+# from api.commands import setup_commands
+
+# # from models import Person
+
+# ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
+# static_file_dir = os.path.join(os.path.dirname(
+#     os.path.realpath(__file__)), '../public/')
+# app = Flask(__name__)
+# app.url_map.strict_slashes = False
+
+# # database condiguration
+# db_url = os.getenv("DATABASE_URL")
+# if db_url is not None:
+#     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+#         "postgres://", "postgresql://")
+# else:
+#     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# MIGRATE = Migrate(app, db, compare_type=True)
+# db.init_app(app)
+
+# # add the admin
+# setup_admin(app)
+
+# # add the admin
+# setup_commands(app)
+
+# # Add all endpoints form the API with a "api" prefix
+# app.register_blueprint(api, url_prefix='/api')
+
+# # Handle/serialize errors like a JSON object
+
+
+# @app.errorhandler(APIException)
+# def handle_invalid_usage(error):
+#     return jsonify(error.to_dict()), error.status_code
+
+# # generate sitemap with all your endpoints
+
+
+# @app.route('/')
+# def sitemap():
+#     if ENV == "development":
+#         return generate_sitemap(app)
+#     return send_from_directory(static_file_dir, 'index.html')
+
+# # any other endpoint will try to serve it like a static file
+# @app.route('/<path:path>', methods=['GET'])
+# def serve_any_other_file(path):
+#     if not os.path.isfile(os.path.join(static_file_dir, path)):
+#         path = 'index.html'
+#     response = send_from_directory(static_file_dir, path)
+#     response.cache_control.max_age = 0  # avoid cache memory
+#     return response
+
+
+# # this only runs if `$ python src/main.py` is executed
+# if __name__ == '__main__':
+#     PORT = int(os.environ.get('PORT', 3001))
+#     app.run(host='0.0.0.0', port=PORT, debug=True)
+
+# src/app.py (modificación)
+
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
@@ -5,13 +80,12 @@ import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_jwt_extended import JWTManager
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
-
-# from models import Person
 
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
@@ -19,7 +93,7 @@ static_file_dir = os.path.join(os.path.dirname(
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# database condiguration
+# database configuration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
@@ -28,6 +102,13 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Configuración JWT
+app.config['JWT_SECRET_KEY'] = os.environ.get(
+    'JWT_SECRET', 'super-secret-key')  # Cambiar en producción
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 86400  # 24 horas en segundos
+jwt = JWTManager(app)
+
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
@@ -57,6 +138,8 @@ def sitemap():
     return send_from_directory(static_file_dir, 'index.html')
 
 # any other endpoint will try to serve it like a static file
+
+
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
