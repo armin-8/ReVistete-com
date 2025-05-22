5351b1cb28ddbb08e01769e63e6be4cf29bb3700
Armincracks <125318350+armin-8@users.noreply.github.com>
2025-05-22 01:03:43 +0000

    nuevos modelos

--- src/api/routes.py
+++ src/api/routes.py
@@ -23,7 +23,7 @@
 # src/api/routes.py (modificación)
 
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User, Product, ProductImage
+from api.models import db, User, Product, ProductImage, Sale
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
